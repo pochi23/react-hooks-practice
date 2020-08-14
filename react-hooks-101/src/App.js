edb98850df93e94e64f8eb496fa544eb55e5b065
YukiKaburagi <yuki.loves.omurice@gmail.com>
2020-08-14 20:27:59 +0900

    my first React Hook with useState

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+      setCount(count - 1);
+  };
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+  const decrement2 = () => {
+      setCount((previousCount) => previousCount - 1);
+  };
+  const reset = () => {
+    setCount(0)
+  }
+  const reset2 = () => {
+    setCount((previousCount) => 0)
+  }
+  const times = () => {
+    setCount(count * 2)
+  }
+  const divide = () => {
+    setCount(count / 2)
+  }
+  // const divide3 = () => {
+  //   if(count % 3 === 0) {
+  //     setCount(count / 3)
+  //   }
+  // }
+  const divide3 = () => setCount(previousCount => {
+    if(previousCount % 3 === 0) {
+      return previousCount / 3
+    } else return previousCount
+  })
   return (
-    <div>
-      This is a template for React app.
-    </div>
+    <React.Fragment>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={reset2}>Reset</button>
+      </div>
+      <div>
+        <button onClick={times}>x2</button>
+      </div>
+      <div>
+        <button onClick={divide}>/2</button>
+      </div>
+      <div>
+        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
