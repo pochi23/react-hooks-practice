f15d21392d74ae655be0b91067a2108b00d6f633
YukiKaburagi <yuki.loves.omurice@gmail.com>
2020-08-15 10:53:10 +0900

    implement component using multiple states

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,69 +1,36 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-      setCount(count - 1);
-  };
-  const increment2 = () => {
-    setCount((previousCount) => previousCount + 1);
-  };
-  const decrement2 = () => {
-      setCount((previousCount) => previousCount - 1);
-  };
+const App = (props) => {
+  const initialStates = {
+    name: "",
+    price: 1000
+  }
+  const [name, setName] = useState(initialStates.name)
+  const [price, setPrice] = useState(initialStates.price)
+  // const [name, setName] = useState(props.name)
+  // const [price, setPrice] = useState(props.price)
+
+
   const reset = () => {
-    setCount(0)
+    setPrice(initialStates.price)
+    setName(initialStates.name)
   }
-  const reset2 = () => {
-    setCount((previousCount) => 0)
-  }
-  const times = () => {
-    setCount(count * 2)
-  }
-  const divide = () => {
-    setCount(count / 2)
-  }
-  // const divide3 = () => {
-  //   if(count % 3 === 0) {
-  //     setCount(count / 3)
-  //   }
-  // }
-  const divide3 = () => setCount(previousCount => {
-    if(previousCount % 3 === 0) {
-      return previousCount / 3
-    } else return previousCount
-  })
+
+
   return (
-    <React.Fragment>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-      </div>
-      <div>
-        <button onClick={reset2}>Reset</button>
-      </div>
-      <div>
-        <button onClick={times}>x2</button>
-      </div>
-      <div>
-        <button onClick={divide}>/2</button>
-      </div>
-      <div>
-        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
-      </div>
-    </React.Fragment>
+    <>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+    </>
   );
 };
+
+// App.defaultProps = {
+//   name: "",
+//   price: 1000
+// }
 
 export default App;
