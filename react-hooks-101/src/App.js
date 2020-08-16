2a6de7b50667ca5b5b7c5ef3faee7a5f39ca0bb6
YukiKaburagi <yuki.loves.omurice@gmail.com>
2020-08-16 16:33:17 +0900

    implement object state

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,36 +1,33 @@
 import React, { useState } from "react";
 
-const App = (props) => {
+const App = () => {
   const initialStates = {
     name: "",
-    price: 1000
-  }
-  const [name, setName] = useState(initialStates.name)
-  const [price, setPrice] = useState(initialStates.price)
-  // const [name, setName] = useState(props.name)
-  // const [price, setPrice] = useState(props.price)
-
-
-  const reset = () => {
-    setPrice(initialStates.price)
-    setName(initialStates.name)
-  }
+    price: 1000,
+  };
+  const [state, setState] = useState(initialStates);
+  const { name, price } = state
 
 
   return (
     <>
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(initialStates)}>Reset</button>
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
 
-// App.defaultProps = {
-//   name: "",
-//   price: 1000
-// }
 
 export default App;
